6347affeff6c843154f2b238828ec2cd2618d714
Helix <bozardhh@hotmail.com>
2024-01-14 18:03:40 +0300

    small refactor

--- app/sudokuGame/createSudoku.js
+++ app/sudokuGame/createSudoku.js
@@ -1,28 +1,98 @@
 export const sudokuLib = {
-  '6': {
+  6: {
     blockRows: 2,
     blockCols: 3,
     emptySlots: 20, //1-20(24)
+    emptyGrid: [
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0],
+    ],
+    charsPool: [1, 2, 3, 4, 5, 6],
+    allRowBorders: [2, 4, 6],
+    allColBorders: [2, 4, 6],
   },
-  '8': {
+  8: {
     blockRows: 2,
     blockCols: 4,
     emptySlots: 30, //1-30(34)
+    emptyGrid: [
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+    ],
+    charsPool: [1, 2, 3, 4, 5, 6, 7, 8],
+    allRowBorders: [2, 4, 6, 8],
+    allColBorders: [4, 8],
   },
-  '9': {
+  9: {
     blockRows: 3,
     blockCols: 3,
-    emptySlots: 40, //1-40(44)
+    emptySlots: 40, //1-44(47)
+    emptyGrid: [
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ],
+    charsPool: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    allRowBorders: [3, 6, 9],
+    allColBorders: [3, 6, 9],
   },
-  '10': {
+  10: {
     blockRows: 2,
     blockCols: 5,
-    emptySlots: 49, //1-45(49)
+    emptySlots: 45, //1-45(49)
+    emptyGrid: [
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ],
+    charsPool: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    allRowBorders: [2, 4, 6, 8, 10],
+    allColBorders: [5, 10],
   },
-  '12': {
+  12: {
     blockRows: 3,
     blockCols: 4,
-    emptySlots: 60, //1-60(64)
+    emptySlots: 66, //1-66(68)
+    emptyGrid: [
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ],
+    charsPool: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+    allRowBorders: [3, 6, 9, 12],
+    allColBorders: [4, 8, 12],
   },
   // '14': {
   //   blockRows: 2,
@@ -41,21 +111,14 @@ export const sudokuLib = {
   // },
 };
 
-export const createSudoku = (cells, emptySlots) => {
-  const generateSudokuGrid = (cells = 9, emptySlots) => {
-    const blockRowsLength = sudokuLib[cells].blockRows;
-    const blockColsLength = sudokuLib[cells].blockCols;
-    if (!emptySlots || emptySlots > sudokuLib[cells].emptySlots) {
-      emptySlots = sudokuLib[cells].emptySlots;
-    }
-    let filledGrid = false;
-
+export var createSudoku = (cells = 9, emptySlots) => {
+  const generateSudokuGrid = () => {
     const fillSudokuGrid = (grid) => {
       const allRowsFilled = grid.every((_, rowIndex) => {
         let filledRow = false;
-        let runsCount = 250;
+        let runsCount = 95;
         while (runsCount && !filledRow) {
-          runsCount -= 1;
+          runsCount--;
           filledRow = fillRow(grid, rowIndex);
         }
         return filledRow && (grid[rowIndex] = filledRow);
@@ -65,84 +128,100 @@ export const createSudoku = (cells, emptySlots) => {
 
     const fillRow = (grid, rowIndex) => {
       const newRow = grid[rowIndex].slice();
-      const rowIsFilled = newRow.every((v, colIndex) => {
-        let chars = allowedChars(grid, newRow, rowIndex, colIndex);
-        return v || chars && (newRow[colIndex] = chars.sort(() => Math.random() - 0.5)[0]);
-      });
-      return rowIsFilled && newRow;
+
+      for (let colIndex = 0; colIndex < cells; colIndex++) {
+        if (!newRow[colIndex]) {
+          const chars = allowedChars(grid, newRow, rowIndex, colIndex);
+
+          if (chars) {
+            newRow[colIndex] = chars[Math.floor(Math.random() * chars.length)];
+          } else {
+            return false;
+          }
+        }
+      }
+      return newRow;
     };
 
     const allowedChars = (grid, currRow, currRowIndex, currColIndex) => {
-      const charsPool = Array.from({length: cells}, (_, i) => i + 1);
-      const rowCoords = generateLibRowOrCol(blockRowsLength);
-      const colCoords = generateLibRowOrCol(blockColsLength);
-
-      // const currCharsCol = grid.reduce((col, row, rowIndex) => {
-      //   (rowIndex !== currRowIndex) && col.push(row[currColIndex]);
-      //   return col;
-      // }, []);
-      const currCharsCol = grid
-        .filter((_, rowIndex) => rowIndex !== currRowIndex)
-        .map(row => row[currColIndex]);
-
-      const currCharsBlock = [];
-
-      for (let i = rowCoords[currRowIndex]; i < rowCoords[currRowIndex] + blockRowsLength; i++) {
-        for (let j = colCoords[currColIndex]; j < colCoords[currColIndex] + blockColsLength; j++) {
+      const charsPool = sudokuLib[cells]['charsPool'];
+      const currChars = currRow.slice();
+      //column
+      for (let i = 0; i < cells; i++) {
+        if (i !== currRowIndex) {
+          currChars.push(grid[i][currColIndex]);
+        }
+      }
+      const startRow =
+        Math.floor(currRowIndex / blockRowsLength) * blockRowsLength;
+      const startCol =
+        Math.floor(currColIndex / blockColsLength) * blockColsLength;
+      //block
+      for (let i = startRow; i < startRow + blockRowsLength; i++) {
+        for (let j = startCol; j < startCol + blockColsLength; j++) {
           if (i === currRowIndex && j === currColIndex) {
             continue;
           }
-          currCharsBlock.push(grid[i][j]);
+          currChars.push(grid[i][j]);
         }
       }
-      // const currChars = Array.from(new Set(currRow.concat(currCharsCol, currCharsBlock)));
-      const currChars = Array.from(new Set([...currRow, ...currCharsCol, ...currCharsBlock]));
       const res = charsPool.filter((v) => !currChars.includes(v));
-      return res.length > 0 ? res : false;
+      return res.length ? res : null;
     };
 
-    //Create array with block start coordinates by rows or columns
-    const generateLibRowOrCol = (blockLength) => {
-      let sum = 0;
-      return Array.from({length: cells / blockLength}, (_, i) => {
-        sum = i ? sum + blockLength : 0;
-        return Array.from({length: blockLength}, () => sum);
-      }).flatMap((v) => v);
-    };
+    const removeSlotsInFilledGrid = (grid) => {
+      let runsCount = emptySlots;
+      const newGrid = grid.map((v) => v.slice());
 
-    const removeSlotsInFilledGrid = () => {
-      const lib = Array.from({length: cells}, (_, i) => i);
-      const userGrid = structuredClone(filledGrid);
-      while (emptySlots > 0 && emptySlots < (cells ** 2)) {
-        const randomRow = lib.sort(() => Math.random() - 0.5)[0];
-        const randomCol = lib.sort(() => Math.random() - 0.5)[0];
-        if (userGrid[randomRow][randomCol]) {
-          emptySlots -= 1;
-          userGrid[randomRow][randomCol] = 0;
+      while (runsCount > 0) {
+        const randomRow = Math.floor(Math.random() * cells);
+        const randomCol = Math.floor(Math.random() * cells);
+
+        if (newGrid[randomRow][randomCol]) {
+          runsCount--;
+          newGrid[randomRow][randomCol] = 0;
         }
       }
-      return userGrid;
+
+      return newGrid;
     };
 
-    let runsCount = 200;
+    let filledGrid;
+    let runsCount = 3150;
     while (runsCount && !filledGrid) {
-      runsCount -= 1;
-      filledGrid = fillSudokuGrid(Array.from({length: cells}, () => Array.from({length: cells}, () => 0)));
+      runsCount--;
+      filledGrid = fillSudokuGrid(emptyGrid.map((v) => v.slice()));
     }
+    filledGrid || generateSudokuGrid();
 
+    const testStrSudoku = filledGrid.join();
     const userGrid = removeSlotsInFilledGrid(filledGrid);
-    const testStrSudoku = filledGrid.flatMap((v) => v).join('');
+    let userGridTestFill;
+    let searchSolutionCount = 200;
 
-    let searchSolutionCount = 250;
     while (searchSolutionCount > 0) {
-      const filledUserGrid = structuredClone(userGrid);
-      fillSudokuGrid(filledUserGrid);
-      if (testStrSudoku !== filledUserGrid.flatMap((v) => v).join('')) {
-        return generateSudokuGrid(cells, emptySlots);
+      userGridTestFill = userGrid.map((v) => v.slice());
+      fillSudokuGrid(userGridTestFill);
+      if (testStrSudoku !== userGridTestFill.join()) {
+        return false;
       }
-      searchSolutionCount -= 1;
+      searchSolutionCount--;
     }
     return [filledGrid, userGrid];
   };
-  return generateSudokuGrid(cells, emptySlots)
+
+  const emptyGrid = sudokuLib[cells]['emptyGrid'];
+  const blockRowsLength = sudokuLib[cells].blockRows;
+  const blockColsLength = sudokuLib[cells].blockCols;
+  if (!emptySlots || emptySlots > sudokuLib[cells].emptySlots) {
+    emptySlots = sudokuLib[cells].emptySlots;
+  }
+
+  let res;
+  let rerunsCount = 100000;
+  while (rerunsCount && !res) {
+    rerunsCount--;
+    res = generateSudokuGrid();
+  }
+  return res || false;
 };
--- app/sudokuGame/insertSudokuElement.js
+++ app/sudokuGame/insertSudokuElement.js
@@ -3,8 +3,13 @@ import {createSudoku, sudokuLib} from "./createSudoku.js";
 
 
 export const insertSudokuElement = (element, newCells, newEmptySlots) => {
-  let gameShell = document.createElement('div');
-  gameShell.id = 'gameShell';
+  let outerGameShell = document.createElement('div');
+  outerGameShell.id = 'outerGameShell';
+  let innerGameShell = document.createElement('div');
+  innerGameShell.id = 'innerGameShell';
+  const navigationBlock = document.createElement('div');
+  navigationBlock.id = 'navigation-block';
+  outerGameShell.append(navigationBlock);
 
   //loading
   const loading = document.createElement('div');
@@ -14,83 +19,61 @@ export const insertSudokuElement = (element, newCells, newEmptySlots) => {
   spin.innerHTML = 'progress_activity';
   spin.classList.add('material-symbols-outlined');
   element.innerHTML = '';
-  gameShell.append(loading, spin);
-  element.append(gameShell);
+  innerGameShell.append(loading, spin);
+  outerGameShell.append(innerGameShell);
+  element.append(outerGameShell);
+
 
   //initial variables
   const cells = newCells || 9;
   const emptySlots = newEmptySlots || sudokuLib[cells]['emptySlots'];
-  const blockRowLength = sudokuLib[cells]['blockRows'];
-  const blockColLength = sudokuLib[cells]['blockCols'];
-  let rowSum = 0;
-  let colSum = 0;
-  const allRowBorders = Array.from({length: cells / blockRowLength}, () => rowSum += blockRowLength);
-  const allColBorders = Array.from({length: cells / blockColLength}, () => colSum += blockColLength);
+  const allRowBorders = sudokuLib[cells]['allRowBorders'];
+  const allColBorders = sudokuLib[cells]['allColBorders'];
   let solutionGrid, initialGrid, userGrid;
   let selectRow = null;
   let selectCol = null;
   let displayCells = cells;
   let wrongAnswerCount = 0;
 
-  const renderGrid = () => {
-    gameShell.innerHTML = '';
 
-    if (wrongAnswerCount > 3) {
-      const gameOver = document.createElement('div');
-      gameOver.style.cssText = 'font-size:30px; margin-bottom:5px';
-      gameOver.innerHTML = 'Game Over';
-      element.innerHTML = '';
-      let newGameButton = document.createElement('button');
-      newGameButton.innerHTML = 'New game';
-      newGameButton.addEventListener('click', () => {
-        insertSudokuElement(element, displayCells);
+  const renderNavigation = () => {
+    navigationBlock.innerHTML = '';
+    //Buttons group: select cells length
+    const buttonsInsertCellsModalBlock = document.createElement('div');
+    buttonsInsertCellsModalBlock.id = 'insertCells-block';
+    const buttonShowModalInsertCellsBlock = document.createElement('button');
+    buttonShowModalInsertCellsBlock.id = 'buttonShowModalInsertCellsBlock';
+    buttonShowModalInsertCellsBlock.textContent = displayCells + '/' + displayCells;
+    buttonShowModalInsertCellsBlock.addEventListener('click', () => {
+      const displayStyle = buttonsInsertCellsModalBlock.style;
+      displayStyle.display === 'flex' ?
+        (displayStyle.display = 'none')
+        : (displayStyle.display = 'flex');
+    });
+
+    Object.keys(sudokuLib).forEach((v) => {
+      const button = document.createElement('button');
+      button.textContent = v + '/' + v;
+      button.addEventListener('click', () => {
+        displayCells = v;
+        renderNavigation();
       });
+      buttonsInsertCellsModalBlock.append(button);
+    });
 
-      gameShell.append(gameOver, newGameButton);
-      element.append(gameShell);
-      return;
-    }
-
-    //New game block
-    const navigationBlock = document.createElement('div');
-    navigationBlock.id = 'navigation-block';
-    {
-      //Buttons group: select cells length
-      const buttonsInsertCellsModalBlock = document.createElement('div');
-      buttonsInsertCellsModalBlock.id = 'insertCells-block';
-      const buttonShowModalInsertCellsBlock = document.createElement('button');
-      buttonShowModalInsertCellsBlock.id = 'buttonShowModalInsertCellsBlock';
-      buttonShowModalInsertCellsBlock.innerHTML = displayCells + '/' + displayCells;
-      buttonShowModalInsertCellsBlock.addEventListener('click', () => {
-        const displayStyle = buttonsInsertCellsModalBlock.style;
-        displayStyle.display === 'flex' ?
-          (displayStyle.display = 'none')
-          : (displayStyle.display = 'flex');
-      });
-
-      Object.keys(sudokuLib).forEach((v) => {
-        const button = document.createElement('button');
-        button.innerHTML = v + '/' + v;
-        button.addEventListener('click', () => {
-          displayCells = v;
-          renderGrid();
-        });
-        buttonsInsertCellsModalBlock.append(button);
-      });
-
-      //New game button
-      const newGameButton = document.createElement('button');
-      newGameButton.innerHTML = 'New game';
-      newGameButton.addEventListener('click', () => {
-        insertSudokuElement(element, displayCells);
-      });
-      const newGameBlock = document.createElement('div');
-      newGameBlock.id = 'newGame-block';
-      newGameBlock.append(newGameButton, buttonShowModalInsertCellsBlock);
+    //New game button
+    const newGameButton = document.createElement('button');
+    newGameButton.textContent = 'New game';
+    newGameButton.addEventListener('click', () => {
+      insertSudokuElement(element, displayCells);
+    });
+    const newGameBlock = document.createElement('div');
+    newGameBlock.id = 'newGame-block';
+    newGameBlock.append(newGameButton, buttonShowModalInsertCellsBlock);
 
 
-      navigationBlock.append(newGameBlock, buttonsInsertCellsModalBlock);
-    }
+    navigationBlock.append(newGameBlock, buttonsInsertCellsModalBlock);
+
 
     //Wrong answers count
     const wrongAnswersElem = document.createElement('div');
@@ -102,17 +85,33 @@ export const insertSudokuElement = (element, newCells, newEmptySlots) => {
     } else if (wrongAnswerCount > 2) {
       wrongAnswersElem.style.background = '#f66d6d';
     }
-    wrongAnswersElem.innerHTML = wrongAnswerCount.toString() + '/3';
+    wrongAnswersElem.textContent = wrongAnswerCount.toString() + '/3';
     navigationBlock.append(wrongAnswersElem);
 
     //Reset button
     const resetButton = document.createElement('button');
     resetButton.id = 'resetButton';
-    resetButton.innerHTML = 'Reset';
+    resetButton.textContent = 'Reset';
     resetButton.addEventListener('click', () => {
-
+      userGrid = initialGrid.map((v) => v.slice());
+      wrongAnswerCount = 0;
+      renderGrid();
     });
     navigationBlock.prepend(resetButton);
+  };
+
+  const renderGrid = () => {
+    innerGameShell.innerHTML = '';
+
+    //game over
+    if (wrongAnswerCount >= 3) {
+      const gameOver = document.createElement('div');
+      gameOver.style.cssText = 'font-size:30px; margin-bottom:5px';
+      gameOver.textContent = 'Game Over';
+      innerGameShell.innerHTML = '';
+      innerGameShell.append(gameOver);
+      return;
+    }
 
     //Sudoku grid element
     const borderGrid = document.createElement('div');
@@ -130,10 +129,10 @@ export const insertSudokuElement = (element, newCells, newEmptySlots) => {
           td.classList.add('gridColBorder');
         }
         if (v) {
-          td.innerHTML = v.toString();
+          td.textContent = v.toString();
         } else {
           const button = document.createElement('button');
-          button.innerHTML = userGrid[rowIndex][colIndex] || ' ';
+          button.textContent = userGrid[rowIndex][colIndex] || ' ';
           button.classList.add('cellButton');
           if (rowIndex === 0 && colIndex === 0) {
             button.classList.add('cellLTR');
@@ -166,7 +165,7 @@ export const insertSudokuElement = (element, newCells, newEmptySlots) => {
     const numberToInsert = Array.from({length: cells}, (_, i) => i + 1);
     numberToInsert.forEach((v) => {
       const button = document.createElement('button');
-      button.innerHTML = v.toString();
+      button.textContent = v.toString();
 
       //insert number to grid
       button.addEventListener('click', (e) => {
@@ -178,20 +177,22 @@ export const insertSudokuElement = (element, newCells, newEmptySlots) => {
         if (userGrid[selectRow][selectCol] !== solutionGrid[selectRow][selectCol]) {
           wrongAnswerCount += 1;
         }
+        renderNavigation();
         renderGrid();
       });
       buttonsInsertNumberBlock.append(button);
     });
     borderGrid.append(table);
-    gameShell.append(navigationBlock, borderGrid, buttonsInsertNumberBlock);
+    innerGameShell.append(borderGrid, buttonsInsertNumberBlock);
     element.innerHTML = '';
-    element.append(gameShell);
+    outerGameShell.append(innerGameShell);
+    element.append(outerGameShell);
   };
 
-
+  renderNavigation();
   setTimeout(() => {
     [solutionGrid, initialGrid] = createSudoku(newCells, emptySlots);
-    userGrid = structuredClone(initialGrid);
+    userGrid = initialGrid.map((v) => v.slice());
     console.log(initialGrid);
     console.log(solutionGrid);
     setTimeout(() => renderGrid(), 500);

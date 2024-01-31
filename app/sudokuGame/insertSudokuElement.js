7709dad726c0375ceea239d61e55253ea4f0b803
helix_x <bozardhh@hotmail.com>
2024-01-31 23:11:30 +0300

    fix rerender

--- app/sudokuGame/insertSudokuElement.js
+++ app/sudokuGame/insertSudokuElement.js
@@ -2,26 +2,6 @@ import './sudokuGrid.css';
 import { createSudoku, sudokuLib } from './createSudoku.js';
 
 export const insertSudokuElement = (element, newCells, newEmptySlots) => {
-  let outerGameShell = document.createElement('div');
-  outerGameShell.id = 'outerGameShell';
-  let innerGameShell = document.createElement('div');
-  innerGameShell.id = 'innerGameShell';
-  const navigationBlock = document.createElement('div');
-  navigationBlock.id = 'navigation-block';
-  outerGameShell.append(navigationBlock);
-
-  //loading
-  const loading = document.createElement('div');
-  loading.style.cssText = 'font-size:30px';
-  loading.innerHTML = 'Loading';
-  const spin = document.createElement('div');
-  spin.innerHTML = 'hourglass_empty';
-  spin.classList.add('material-symbols-outlined');
-  element.innerHTML = '';
-  innerGameShell.append(loading, spin);
-  outerGameShell.append(innerGameShell);
-  element.append(outerGameShell);
-
   //initial variables
   const cells = newCells || 9;
   const emptySlots = newEmptySlots || sudokuLib[cells]['emptySlots'];
@@ -34,49 +14,91 @@ export const insertSudokuElement = (element, newCells, newEmptySlots) => {
   let displayCells = cells;
   let wrongAnswerCount = 0;
   let gameOverText;
-
-  const renderNavigation = () => {
-    navigationBlock.innerHTML = '';
-    //Buttons group: select cells length
-    const buttonsInsertCellsModalBlock = document.createElement('div');
-    buttonsInsertCellsModalBlock.id = 'insertCells-block';
-    const buttonShowModalInsertCellsBlock = document.createElement('button');
-    buttonShowModalInsertCellsBlock.id = 'buttonShowModalInsertCellsBlock';
-    buttonShowModalInsertCellsBlock.textContent =
-      displayCells + '/' + displayCells;
-    buttonShowModalInsertCellsBlock.addEventListener('click', () => {
-      const displayStyle = buttonsInsertCellsModalBlock.style;
-      displayStyle.display === 'flex'
-        ? (displayStyle.display = 'none')
-        : (displayStyle.display = 'flex');
+  //DOM
+  const outerGameShell = document.createElement('div');
+  outerGameShell.id = 'outerGameShell';
+  const innerGameShell = document.createElement('div');
+  innerGameShell.id = 'innerGameShell';
+  const navigationBlock = document.createElement('div');
+  navigationBlock.id = 'navigation-block';
+  const buttonsInsertNumberBlock = document.createElement('div');
+  buttonsInsertNumberBlock.id = 'insertNumberButtons-block';
+  //Buttons group: select cells length
+  const selectCellsModalBlock = document.createElement('div');
+  selectCellsModalBlock.id = 'selectCells-block';
+  const showModalButton = document.createElement('button');
+  showModalButton.id = 'showModalButton';
+  showModalButton.innerHTML = `<span>${
+    displayCells + '/' + displayCells
+  }<span/>`;
+  showModalButton.addEventListener('click', (e) => {
+    e.preventDefault;
+    if (!showModalButton.classList.contains('activeButton')) {
+      showModalButton.classList.add('activeButton');
+      setTimeout(() => showModalButton.classList.remove('activeButton'), 300);
+    }
+    const displayStyle = selectCellsModalBlock.style;
+    displayStyle.display === 'flex'
+      ? (displayStyle.display = 'none')
+      : (displayStyle.display = 'flex');
+  });
+  document.addEventListener('click', (e) => {
+    const clickInsideButton = showModalButton.contains(e.target);
+    const clickInsideModal = selectCellsModalBlock.contains(e.target);
+    if (!clickInsideButton && !clickInsideModal) {
+      selectCellsModalBlock.style.display = 'none';
+    }
+  });
+  Object.keys(sudokuLib).forEach((v) => {
+    const button = document.createElement('button');
+    button.textContent = v + '/' + v;
+    button.addEventListener('click', () => {
+      displayCells = v;
+      showModalButton.innerHTML = `<span>${
+        displayCells + '/' + displayCells
+      }<span/>`;
+      selectCellsModalBlock.style.display = 'none';
     });
+    selectCellsModalBlock.append(button);
+  });
 
-    Object.keys(sudokuLib).forEach((v) => {
-      const button = document.createElement('button');
-      button.textContent = v + '/' + v;
-      button.addEventListener('click', () => {
-        displayCells = v;
-        renderNavigation();
-      });
-      buttonsInsertCellsModalBlock.append(button);
-    });
-
-    //New game button
-    const newGameButton = document.createElement('button');
-    newGameButton.innerHTML = '<span>New Game</span>';
-    newGameButton.addEventListener('click', () => {
-      insertSudokuElement(element, displayCells);
-    });
-    const newGameBlock = document.createElement('div');
-    newGameBlock.id = 'newGame-block';
-    newGameBlock.append(newGameButton, buttonShowModalInsertCellsBlock);
-
-    navigationBlock.append(newGameBlock, buttonsInsertCellsModalBlock);
-
-    //Wrong answers count
-    const wrongAnswersElem = document.createElement('div');
-    wrongAnswersElem.id = 'wrongAnswersElem';
-    if (wrongAnswerCount === 1) {
+  //New game button
+  const newGameButton = document.createElement('button');
+  newGameButton.innerHTML = '<span>New Game</span>';
+  newGameButton.addEventListener('click', () => {
+    if (!newGameButton.classList.contains('activeButton')) {
+      newGameButton.classList.add('activeButton');
+      setTimeout(() => insertSudokuElement(element, displayCells), 200);
+    }
+  });
+  const newGameBlock = document.createElement('div');
+  newGameBlock.id = 'newGame-block';
+  //Wrong answers elem
+  const wrongAnswersElem = document.createElement('div');
+  wrongAnswersElem.id = 'wrongAnswersElem';
+  //Reset button
+  const resetButton = document.createElement('button');
+  resetButton.id = 'resetButton';
+  resetButton.innerHTML = '<span>Reset</span>';
+  resetButton.addEventListener('click', (e) => {
+    e.preventDefault;
+    if (!resetButton.classList.contains('activeButton')) {
+      resetButton.classList.add('activeButton');
+      setTimeout(() => resetButton.classList.remove('activeButton'), 300);
+    }
+    if (gameOverText) {
+      return;
+    }
+    userGrid = initialGrid.map((v) => v.slice());
+    wrongAnswerCount = 0;
+    renderGrid();
+    changeWrongAnswers();
+  });
+  //
+  const changeWrongAnswers = () => {
+    if (wrongAnswerCount === 0) {
+      wrongAnswersElem.style.background = 'var(--mc-6)';
+    } else if (wrongAnswerCount === 1) {
       wrongAnswersElem.style.background = 'var(--erc-1)';
     } else if (wrongAnswerCount === 2) {
       wrongAnswersElem.style.background = 'var(--erc-2)';
@@ -84,70 +106,105 @@ export const insertSudokuElement = (element, newCells, newEmptySlots) => {
       wrongAnswersElem.style.background = 'var(--erc-3)';
     }
     wrongAnswersElem.textContent = wrongAnswerCount.toString() + '/3';
-    navigationBlock.append(wrongAnswersElem);
-
-    //Reset button
-    const resetButton = document.createElement('button');
-    if (wrongAnswerCount > 2) {
-      resetButton.disabled = true;
+  };
+  //Render navigation
+  newGameBlock.append(newGameButton, showModalButton);
+  navigationBlock.append(newGameBlock, selectCellsModalBlock);
+  navigationBlock.append(wrongAnswersElem);
+  navigationBlock.prepend(resetButton);
+  changeWrongAnswers();
+  //Render buttons group: chose numbers to insert
+  const renderInsertNumberButtons = () => {
+    buttonsInsertNumberBlock.innerHTML = '';
+    if (!numberToInsert) {
+      return;
     }
-    resetButton.id = 'resetButton';
-    resetButton.innerHTML = '<span>Reset</span>';
-    resetButton.addEventListener('click', () => {
-      userGrid = initialGrid.map((v) => v.slice());
-      wrongAnswerCount = 0;
-      renderGrid();
-      renderNavigation();
+    numberToInsert.forEach((v) => {
+      const button = document.createElement('button');
+      button.innerHTML = `<span>${v.toString()}</span>`;
+      //insert number to grid
+      button.addEventListener('click', (e) => {
+        e.preventDefault();
+        if (!button.classList.contains('activeButton')) {
+          button.classList.add('activeButton');
+          setTimeout(() => button.classList.remove('activeButton'), 300);
+        }
+        if (selectRow === null || selectCol === null) {
+          return;
+        }
+        userGrid[selectRow][selectCol] = Number(v);
+        if (
+          userGrid[selectRow][selectCol] !== solutionGrid[selectRow][selectCol]
+        ) {
+          wrongAnswerCount += 1;
+        }
+        changeWrongAnswers();
+        renderGrid();
+      });
+      buttonsInsertNumberBlock.append(button);
+      outerGameShell.append(buttonsInsertNumberBlock);
     });
-    navigationBlock.prepend(resetButton);
+  };
+
+  const renderGridSkeleton = () => {
+    innerGameShell.innerHTML = '';
+    //loading
+    const loading = document.createElement('div');
+    loading.classList.add('loading');
+    loading.innerHTML = '<span>Loading<span/>';
+    const spin = document.createElement('div');
+    spin.innerHTML = 'hourglass_empty';
+    spin.classList.add('material-symbols-outlined');
+    loading.append(spin);
+    //
+    const borderGrid = document.createElement('div');
+    borderGrid.id = 'borderGrid';
+    const table = document.createElement('table');
+    const grid = sudokuLib[cells]['emptyGrid'];
+    grid.forEach((row, rowIndex) => {
+      const tr = document.createElement('tr');
+      if (allRowBorders.includes(rowIndex)) {
+        tr.classList.add('gridRowBorder');
+      }
+      row.forEach((_, colIndex) => {
+        const td = document.createElement('td');
+        if (allColBorders.includes(colIndex)) {
+          td.classList.add('gridColBorder');
+        }
+        td.textContent = '';
+        tr.appendChild(td);
+      });
+      table.appendChild(tr);
+    });
+    const gameCompleted = document.createElement('div');
+    gameCompleted.classList.add('gameOver');
+    gameCompleted.append(loading);
+    const border2grid = document.createElement('div');
+    border2grid.id = 'border2grid';
+    const shadowGrid = document.createElement('div');
+    shadowGrid.id = 'shadowGrid';
+    !numberToInsert && gameOver();
+    border2grid.append(gameCompleted, shadowGrid, table);
+    borderGrid.append(border2grid);
+    innerGameShell.append(borderGrid);
   };
 
   const renderGrid = () => {
     innerGameShell.innerHTML = '';
-
     //Game over
     if (wrongAnswerCount >= 3) {
       numberToInsert = null;
       gameOverText = 'Game Over';
+      renderInsertNumberButtons();
     }
-
     //Sudoku completed
     if (solutionGrid.join() === userGrid.join()) {
       numberToInsert = null;
       gameOverText = 'Congratulations! You completed the Sudoku!';
       selectCol = null;
       selectRow = null;
+      renderInsertNumberButtons();
     }
-
-    //Buttons group: chose numbers to insert
-    const renderInsertNumberButtons = () => {
-      const buttonsInsertNumberBlock = document.createElement('div');
-      buttonsInsertNumberBlock.id = 'insertNumberButtons-block';
-      numberToInsert.forEach((v) => {
-        const button = document.createElement('button');
-        button.innerHTML = `<span>${v.toString()}</span>`;
-
-        //insert number to grid
-        button.addEventListener('click', (e) => {
-          e.preventDefault();
-          if (selectRow === null || selectCol === null) {
-            return;
-          }
-          userGrid[selectRow][selectCol] = Number(v);
-          if (
-            userGrid[selectRow][selectCol] !==
-            solutionGrid[selectRow][selectCol]
-          ) {
-            wrongAnswerCount += 1;
-          }
-          renderNavigation();
-          renderGrid();
-        });
-        buttonsInsertNumberBlock.append(button);
-        innerGameShell.append(buttonsInsertNumberBlock);
-      });
-    };
-
     //Sudoku grid element
     const borderGrid = document.createElement('div');
     borderGrid.id = 'borderGrid';
@@ -202,35 +259,35 @@ export const insertSudokuElement = (element, newCells, newEmptySlots) => {
       table.appendChild(tr);
     });
 
-    const renderGameOver = () => {
+    const gameOver = () => {
       const gameCompleted = document.createElement('div');
       gameCompleted.classList.add('gameOver');
-      const text = document.createElement('span');
-      text.textContent = gameOverText;
-      gameCompleted.append(text);
+      gameCompleted.innerHTML = `<span>${gameOverText}</span>`;
       border2grid.append(gameCompleted);
     };
-
-    const shadowGrid = document.createElement('div');
-    shadowGrid.id = 'shadowGrid';
     const border2grid = document.createElement('div');
     border2grid.id = 'border2grid';
-    numberToInsert ? renderInsertNumberButtons() : renderGameOver();
-    border2grid.append(shadowGrid);
-    border2grid.append(table);
+    const shadowGrid = document.createElement('div');
+    shadowGrid.id = 'shadowGrid';
+    !numberToInsert && gameOver();
+    border2grid.append(shadowGrid, table);
     borderGrid.append(border2grid);
-    innerGameShell.prepend(borderGrid);
-    element.innerHTML = '';
-    outerGameShell.append(innerGameShell);
-    element.append(outerGameShell);
+    innerGameShell.append(borderGrid);
   };
-
-  renderNavigation();
+  element.innerHTML = '';
+  element.append(outerGameShell);
+  outerGameShell.append(navigationBlock, innerGameShell);
+  renderGridSkeleton();
+  renderInsertNumberButtons();
   setTimeout(() => {
     [solutionGrid, initialGrid] = createSudoku(cells, emptySlots);
     userGrid = initialGrid.map((v) => v.slice());
     console.log(initialGrid);
     console.log(solutionGrid);
-    setTimeout(() => renderGrid(), 500);
+    3;
+    setTimeout(() => {
+      renderGrid(false);
+      renderInsertNumberButtons();
+    }, 500);
   }, 500);
 };
